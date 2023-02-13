f115cf1f8c995bc45251b9c5da2a2aafe98a9808
Yamil Ortega <yamil.israel.ortega@pemex.com>
2023-02-13 13:29:08 -0600

    Se ajusta deployment para ingress

--- 05-MongoDbDeploymentWithIngress/01EnableMongodbWithIngress.bat
+++ 05-MongoDbDeploymentWithIngress/01EnableMongodbWithIngress.bat
@@ -3,3 +3,4 @@ kubectl apply -f 06-mongoDBSecretWithIngress.yaml
 kubectl apply -f 07-mongoDBDeploymentWithIngress.yaml
 kubectl apply -f 08-mongoExpressConfigMapWithIngress.yaml
 kubectl apply -f 09-mongoExpressDeploymentWithIngress.yaml
+kubectl apply -f 10-IngressRule.yaml
