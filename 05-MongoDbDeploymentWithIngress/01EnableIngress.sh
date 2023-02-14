1674a831e43faf2ae1be42c0d94db208b5f3cb1f
Yamil Ortega <yamil.israel.ortega@pemex.com>
2023-02-14 13:32:13 -0600

    Se agrega archivo de instalacion cluster maquinas virtuales

--- 05-MongoDbDeploymentWithIngress/01EnableIngress.sh
+++ 05-MongoDbDeploymentWithIngress/01EnableIngress.sh
@@ -1,1 +1,13 @@
+curl -fsSL -o get_helm.sh https://raw.githubusercontent.com/helm/helm/main/scripts/get-helm-3
+chmod 700 get_helm.sh
+./get_helm.sh
+helm repo add ingress-nginx https://kubernetes.github.io/ingress-nginx
+
+
+helm repo update
+helm repo list
+
+helm upgrade --install myingress ingress-nginx/ingress-nginx --namespace ingress-nginx --create-namespace --values values.yml
+
+
 kubectl apply -f 05Ingress.yaml
