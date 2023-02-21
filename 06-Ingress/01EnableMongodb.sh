4e431862b1d1ad4888d1a4713ebbd63a9679baf4
Yamil Ortega <elcorreodeyamil@gmail.com>
2023-02-21 10:44:32 -0600

    Se agregan archivos de deployment a carpeta

---
+++ 06-Ingress/01EnableMongodb.sh
@@ -0,0 +1,4 @@
+kubectl apply -f 06-mongoDBSecret.yaml
+kubectl apply -f 07-mongoDBDeployment.yaml
+kubectl apply -f 08-mongoExpressConfigMap.yaml
+kubectl apply -f 09-mongoExpressDeployment.yaml
---
+++ 06-Ingress/02DisableMongodb.bat
@@ -0,0 +1,4 @@
+kubectl delete -f 06-mongoDBSecret.yaml
+kubectl delete -f 07-mongoDBDeployment.yaml
+kubectl delete -f 08-mongoExpressConfigMap.yaml
+kubectl delete -f 09-mongoExpressDeployment.yaml
