50fe1d7bae9cd856914bcede264f57b8bb2860f2
Yamil Ortega <elcorreodeyamil@gmail.com>
2023-03-02 15:33:27 -0600

    Se cambia nombre de archivo

--- 06-Ingress/00EnableIngress.sh
+++ 06-Ingress/00EnableIngress.sh
@@ -5,4 +5,4 @@ helm repo update
 helm search repo ingress
 
 kubectl create ns ingress-nginx
-helm install nginx-ingress ingress-nginx/ingress-nginx -n ingress-nginx --values ingress_configuration.yaml 
+helm install nginx-ingress ingress-nginx/ingress-nginx -n ingress-nginx --values 00ingress_configuration.yaml 
