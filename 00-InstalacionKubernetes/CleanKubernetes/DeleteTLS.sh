ed48342f2d4a2e531b54290dad386e4c39b599ce
Yamil Ortega <elcorreodeyamil@gmail.com>
2023-03-03 17:18:38 -0600

    Se agregan archivos adiciones

---
+++ 00-InstalacionKubernetes/CleanKubernetes/DeleteTLS.sh
@@ -0,0 +1,3 @@
+kubectl delete namespaces cert-manager
+kubectl delete namespaces ingress-nginx
+kubectl delete namespaces metallb-system
--- 00-InstalacionKubernetes/metallb/01InstallMetallLb.sh
+++ 00-InstalacionKubernetes/metallb/01InstallMetallLb.sh
@@ -1,1 +1,2 @@
-kubectl apply -f https://raw.githubusercontent.com/metallb/metallb/v0.13.7/config/manifests/metallb-native.yaml
+#kubectl apply -f https://raw.githubusercontent.com/metallb/metallb/v0.13.7/config/manifests/metallb-native.yaml
+kubectl apply -f https://raw.githubusercontent.com/metallb/metallb/v0.13.9/config/manifests/metallb-native.yaml
