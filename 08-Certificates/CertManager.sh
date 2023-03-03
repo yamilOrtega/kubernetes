63821d681a07be5653a3ae1635999365049d487f
Yamil Ortega <elcorreodeyamil@gmail.com>
2023-03-03 11:38:02 -0600

    se actualiza cert-manager a version ultima estable

--- 08-Certificates/CertManager.sh
+++ 08-Certificates/CertManager.sh
@@ -1,2 +1,5 @@
 kubectl create ns cert-manager
 kubectl apply --validate=false -f cert-manager.yaml
+#Info https://cert-manager.io/docs/installation/kubectl/
+#kubectl apply -f https://github.com/cert-manager/cert-manager/releases/download/v1.11.0/cert-manager.yaml
+#kubectl delete -f https://github.com/cert-manager/cert-manager/releases/download/vX.Y.Z/cert-manager.yaml
