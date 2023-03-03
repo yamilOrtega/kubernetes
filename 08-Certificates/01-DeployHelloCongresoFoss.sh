7c1c92cb441a7d6b96ccb5e129e025a3e18962b7
Yamil Ortega <elcorreodeyamil@gmail.com>
2023-03-03 17:25:21 -0600

    Se agregan archivos

--- 08-Certificates/01-DeployHelloCongresoFoss.sh
+++ 08-Certificates/01-DeployHelloCongresoFoss.sh
@@ -1,4 +1,2 @@
 kubectl apply -f 01-hello-app.yaml
 kubectl apply -f 02-hello-ingress.yaml
-kubectl apply -f 03-issuer.yaml
-kubectl apply -f 04-hello-ingress-tls.yaml
---
+++ 08-Certificates/02-DeployHelloCongresoFoss.sh
@@ -0,0 +1,4 @@
+kubectl apply -f 01-hello-app.yaml
+kubectl delete -f 02-hello-ingress.yaml
+kubectl apply -f 03-issuer.yaml
+kubectl apply -f 04-hello-ingress-tls.yaml
