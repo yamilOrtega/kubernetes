a94beccfd95ce5c1104e7ad74a4c1d4ffd983198
Yamil Ortega <elcorreodeyamil@gmail.com>
2023-03-06 12:40:18 -0600

    Se quita mensaje de monitoreo

--- 00-InstalacionKubernetes/monitoring/prometheus.sh
+++ 00-InstalacionKubernetes/monitoring/prometheus.sh
@@ -2,5 +2,6 @@ kubectl create namespace monitoring
 kubectl apply -f 00-clusterRole.yaml
 kubectl apply -f 01-config-map.yaml
 kubectl apply -f 02-prometheus-deployment.yaml
-#Se revisan estatus de cracion
-kubectl get deployments --namespace=monitoring -w
+kubectl apply -f 03-prometheus-service.yaml 
+kubectl apply -f 04-prometheus-ingress.yaml
+
