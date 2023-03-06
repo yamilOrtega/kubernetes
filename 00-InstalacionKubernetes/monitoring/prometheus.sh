982181b73a9f2181118ec54a15b851390eeff611
Yamil Ortega <elcorreodeyamil@gmail.com>
2023-03-06 11:49:48 -0600

    Se agrega servicio e ingress

--- 00-InstalacionKubernetes/monitoring/prometheus.sh
+++ 00-InstalacionKubernetes/monitoring/prometheus.sh
@@ -1,6 +1,5 @@
 kubectl create namespace monitoring
 kubectl apply -f 00-clusterRole.yaml
-kubectl apply -f clusterRole.yaml
 kubectl apply -f 01-config-map.yaml
 kubectl apply -f 02-prometheus-deployment.yaml
 #Se revisan estatus de cracion
