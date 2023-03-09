0a5604ed3246b4dfa74439ca3b9872d4d05997c3
Yamil Ortega <elcorreodeyamil@gmail.com>
2023-03-09 14:15:42 -0600

    Prueba 1 de registro de container registry

---
+++ 09-Congresofossf/00-deploycongresofoss.sh
@@ -0,0 +1,11 @@
+#Create ACR on azure container registry
+#docker login -u kubernetescongresofoss -p nD7cPWzfq9knoAnelM+nA4PvY+E239Xm8U1QhKaCwd+ACRAfhtGO congresofosscr.azurecr.io
+#nD7cPWzfq9knoAnelM+nA4PvY+E239Xm8U1QhKaCwd+ACRAfhtGO
+
+kubectl create secret docker-registry acr-auth \
+    --docker-server=congresofosscr.azurecr.io \
+    --docker-username=kubernetescongresofoss \
+    --docker-password=nD7cPWzfq9knoAnelM+nA4PvY+E239Xm8U1QhKaCwd+ACRAfhtGO
+      #--docker-email=<your-acr-email>
+#kubectl apply -f 01-congresofoss-app.yaml
+    
