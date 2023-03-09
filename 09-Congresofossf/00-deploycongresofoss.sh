9c3c16a8a94d8e8d75274c0d42af9fc2e60b76c5
Yamil Ortega <elcorreodeyamil@gmail.com>
2023-03-09 14:27:42 -0600

    Se agrega ingress

--- 09-Congresofossf/00-deploycongresofoss.sh
+++ 09-Congresofossf/00-deploycongresofoss.sh
@@ -6,6 +6,10 @@ kubectl create secret docker-registry acr-auth \
     --docker-server=congresofosscr.azurecr.io \
     --docker-username=kubernetescongresofoss \
     --docker-password=nD7cPWzfq9knoAnelM+nA4PvY+E239Xm8U1QhKaCwd+ACRAfhtGO
+#Se agrega al service acount default la descarga del secret 
+kubectl patch serviceaccount default \
+  -p '{"imagePullSecrets": [{"name": "acr-auth"}]}'
+
       #--docker-email=<your-acr-email>
 #kubectl apply -f 01-congresofoss-app.yaml
     
