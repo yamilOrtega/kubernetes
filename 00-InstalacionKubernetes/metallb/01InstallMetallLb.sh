0d2687fc7914e5295545aa10657af963b1035d3b
Yamil Ortega <elcorreodeyamil@gmail.com>
2023-09-19 10:58:38 -0600

    Se actualiza version de metallb

--- 00-InstalacionKubernetes/metallb/01InstallMetallLb.sh
+++ 00-InstalacionKubernetes/metallb/01InstallMetallLb.sh
@@ -1,2 +1,3 @@
 #kubectl apply -f https://raw.githubusercontent.com/metallb/metallb/v0.13.7/config/manifests/metallb-native.yaml
-kubectl apply -f https://raw.githubusercontent.com/metallb/metallb/v0.13.9/config/manifests/metallb-native.yaml
+#kubectl apply -f https://raw.githubusercontent.com/metallb/metallb/v0.13.9/config/manifests/metallb-native.yaml
+kubectl apply -f https://raw.githubusercontent.com/metallb/metallb/v0.13.11/config/manifests/metallb-native.yaml
