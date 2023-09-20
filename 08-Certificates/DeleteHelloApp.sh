18948375c97368ff77ef29fca4d22bc2fe77b5b6
Yamil Ortega <elcorreodeyamil@gmail.com>
2023-09-20 10:12:39 -0600

    Se agrega borrado de certificate

--- 08-Certificates/DeleteHelloApp.sh
+++ 08-Certificates/DeleteHelloApp.sh
@@ -1,4 +1,5 @@
 kubectl delete -f 04-hello-ingress-tls.yaml
+kubectl delete -f 03.1-Certificate.yaml
 kubectl delete -f 03-issuer.yaml
 kubectl delete -f 02-hello-ingress.yaml
 kubectl delete -f 01-hello-app.yaml
