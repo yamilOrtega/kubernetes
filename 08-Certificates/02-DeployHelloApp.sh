fdca0428ae23369a361a971ac296e6129625aa5f
Yamil Ortega <elcorreodeyamil@gmail.com>
2023-09-20 18:23:48 -0600

    se arregla contenedor usado

--- 08-Certificates/02-DeployHelloApp.sh
+++ 08-Certificates/02-DeployHelloApp.sh
@@ -2,4 +2,4 @@ kubectl apply -f 01-hello-app.yaml
 kubectl delete -f 02-hello-ingress.yaml
 kubectl apply -f 03-issuer.yaml
 kubectl apply -f 03.1-Certificate.yaml
-kubectl apply -f 04-hello-ingress-tls.yaml
+kubectl apply -f 04-hello-ingress-tls.yaml 
--- 09-Congresofossf/00-deletecongresofoss.sh
+++
@@ -1,4 +0,0 @@
-kubectl delete -f 02-congresofoss-ingress.yaml
-kubectl delete -f 04-congresofoss-ingress-tls.yaml
-kubectl delete namespace congresofoss-namespace
-kubectl delete secret acr-auth
