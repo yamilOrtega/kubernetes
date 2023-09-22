4eb297fa77269eb2605c30b991b443ebb72a7320
Yamil Ortega <elcorreodeyamil@gmail.com>
2023-09-22 17:44:45 -0600

    se agregan scripts de loadbalancer

--- 07-Volumes/nfs/DeleteNginxWithPersistantVolume.sh
+++ 07-Volumes/nfs/DeleteNginxWithPersistantVolume.sh
@@ -1,4 +1,4 @@
-
+kubectl delete -f 04LoadBalancerServiceForNginxShared.yaml:
 kubectl delete -f 03NginxWithPersistentVolumeClaim.yaml
 kubectl delete -f 02PersistentVolumeClaim.yaml
 kubectl delete -f 01PersistentVolume.yaml
--- 07-Volumes/nfs/DeployNginxWithPersistantVolume.sh
+++ 07-Volumes/nfs/DeployNginxWithPersistantVolume.sh
@@ -1,4 +1,5 @@
 kubectl apply -f 01PersistentVolume.yaml
 kubectl apply -f 02PersistentVolumeClaim.yaml
 kubectl apply -f 03NginxWithPersistentVolumeClaim.yaml
+kubectl apply -f 04LoadBalancerServiceForNginxShared.yaml
 
